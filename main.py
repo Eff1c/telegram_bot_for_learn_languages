ce1a66c8087be44281378ed943a3eef615741987
eff1c <illymartynyk@gmail.com>
2021-12-27 20:32:38 +0200

    Create bot with start handler

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
-import asyncio
-from aiogram import Bot
+import logging
+
+from aiogram import Bot, Dispatcher, executor, types
 from decouple import config
 
-# async def main():
-#     bot = Bot(token=BOT_TOKEN)
-#
-#     try:
-#         me = await bot.get_me()
-#         print(f"🤖 Hello, I'm {me.first_name}.\nHave a nice Day!")
-#     finally:
-#         await bot.close()
-#
-# asyncio.run(main())
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# Initialize bot and dispatcher
+bot = Bot(token=config('BOT_TOKEN'))
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    """
+    This handler will be called when user sends `/start` or `/help` command
+    """
+    with open("help.txt", "r", encoding="utf-8") as f:
+        help_text = f.read()
+
+    await message.reply(help_text)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
