96c8527159a3773861e7a1215fb5e09159917e0d
eff1c <illymartynyk@gmail.com>
2021-12-29 08:50:26 +0200

    Refactor main file

--- main.py
+++ main.py
@@ -1,28 +1,30 @@
+import asyncio
 import logging
 
-from aiogram import Bot, Dispatcher, types
-from aiogram.client.session.aiohttp import AiohttpSession
+from aiogram import Bot, Dispatcher, types, Router
 from decouple import config
 
-# Configure logging
 from helpers import read_help_text
 
 logging.basicConfig(level=logging.INFO)
 
-session = AiohttpSession()
-bot = Bot(token=config('BOT_TOKEN'))
-dp = Dispatcher(bot)
+main_router = Router()
 
 
-@dp.message(commands=['start', 'help'])
+@main_router.message(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
-    """
-    This handler will be called when user sends `/start` or `/help` command
-    """
     help_text = read_help_text("main_help.txt")
 
     await message.reply(help_text)
 
 
+async def main():
+    bot = Bot(token=config('BOT_TOKEN'), parse_mode="HTML")
+    dp = Dispatcher()
+    dp.include_router(main_router)
+
+    await dp.start_polling(bot, skip_updates=True)
+
+
 if __name__ == '__main__':
-    dp.start_polling(bot, skip_updates=True)
+    asyncio.run(main())
